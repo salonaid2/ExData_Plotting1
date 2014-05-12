47f053913a0d07ca2b8582efb150495c4ec65e91
saloni <saloninaidu.rekapalli@techmahindra.com>
2014-05-12 11:33:19 +0530

    Fourth Plot

---
+++ plot4.R
@@ -0,0 +1,56 @@
+#Setting working directory
+
+setwd("D:\\<removed path>\\ExploratoryDataAnalysis")
+
+# Creating a new directory
+
+if (!file.exists("proj1")){dir.create("proj1")}
+
+# Downloading the data from the link provided in the course project
+
+fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+download.file(fileUrl, destfile="./proj1/proj1.zip")
+
+list.files("./proj1")
+
+# Reading data into R
+
+data<-read.table(unz("./proj1.zip","household_power_consumption.txt"),
+                 header= TRUE, sep=";", quote="", dec=".", na.strings="?",
+                 colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+head(data)
+
+sapply(data1,class)
+
+# Subsetting the data
+
+data1 <- data[which(data$Date=="1/2/2007"|data$Date=="2/2/2007"),]
+
+table(data$Date) # frequency of records in each date
+
+rm(data) # remove bigger data to optimize memory 
+
+#  Converting the DateTime variables to its appropriate classes
+
+data1$DateTime <-strptime(paste(data1$Date, data1$Time), "%d/%m/%Y %H:%M:%S")
+
+head(data1,2)
+
+# Plotting the fourth graph in the PNG graphics device 
+
+png(file="./proj1/ExData_Plotting1/plot4.png",width=480,height=480, units="px")
+
+par(mfrow=c(2,2))
+with(data1,{
+      plot(DateTime,Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+      plot(DateTime,Voltage ,type="l",xlab="datetime",ylab="Voltage")
+      plot(data1$DateTime,data1$Sub_metering_1, col="black",type="l", xlab="",ylab="Energy Sub metering")
+      lines(data1$DateTime,data1$Sub_metering_2, col="red")
+      lines(data1$DateTime,data1$Sub_metering_3, col="blue")
+      legend ("topright",lty=1,bty="n",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+      plot(DateTime,Global_reactive_power ,type="l",xlab="datetime",ylab="Global_reactive_power")
+})
+    
+dev.off()
